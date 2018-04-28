2c14cae0a2153dcca8f8b5bc7904bb6799bd0b9a
Hung Le <hungle.info@gmail.com>
2018-04-28 13:13:41 +0700

    Split api and service

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -14,6 +14,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toArray';
 import 'rxjs/add/operator/count';
 import 'rxjs/add/operator/single';
+import { AppService } from './app.service';
 
 interface User {
   id: number;
@@ -42,22 +43,29 @@ export class AppController implements OnModuleInit {
   private readonly client: ClientGrpc;
   private userService: UserService;
 
+  constructor(private readonly appService: AppService) {}
+
   onModuleInit(): any {
     this.userService = this.client.getService<UserService>('UserService');
   }
 
-  @Get()
+  @Get('/users')
   async getUsers(): Promise<User[]> {
     const results = this.userService.find({});
     return await results.map(v => (v.users ? v.users : [])).toPromise();
   }
 
   // prettier-ignore
-  @Get(':id')
+  @Get('/users/:id')
   async getUserById(@Param('id', new ParseIntPipe()) id): Promise<User> {
     return await this.userService
       .findOne({ id: parseInt(id, 10) })
       .map(v => v.user ? v.user : {})
       .toPromise();
+  }
+
+  @Get('/')
+  root() {
+    return this.appService.root();
   }
 }
