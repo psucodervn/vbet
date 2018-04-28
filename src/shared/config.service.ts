2c1b1a32e682418932c4c2befed55058d2fd01b5
Hung Le <hungle.info@gmail.com>
2018-04-28 15:19:09 +0700

    Add logger to ConfigService

--- src/shared/config.service.ts
+++ src/shared/config.service.ts
@@ -1,15 +1,20 @@
 import * as fs from 'fs';
 import * as yaml from 'js-yaml';
+import { Logger } from '@nestjs/common';
 
 export class ConfigService {
-  private readonly envConfig;
+  private readonly config;
+  private readonly logger: Logger = new Logger(ConfigService.name);
 
   constructor(filePath: string) {
-    this.envConfig = yaml.safeLoad(fs.readFileSync(filePath));
-    console.log(this.envConfig);
+    try {
+      this.config = yaml.safeLoad(fs.readFileSync(filePath));
+    } catch (e) {
+      this.logger.error(e);
+    }
   }
 
   get(key: string): string {
-    return this.envConfig[key];
+    return this.config[key];
   }
 }
